987c5c70adb357fb9f90b0ff22b95b72d2114cb0
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-11-17 11:35:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  array.each do |array|
 end
+
+def square_array(numbers)
+  new_numbers = []
+  new_numbers.each do |num|
+  new_numbers << (num ** 2)
+end
+  new_numbers  
+end 
+end 
+ 
